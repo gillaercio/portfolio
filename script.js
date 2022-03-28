c0580e06d7159633944eb5396945384ace69ecc3
Gil Laércio <97790358+gillaercio@users.noreply.github.com>
2022-03-28 19:51:41 -0300

    :package: calculo de orcamento

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+// LISTENERS FOR EVENTS
+document.querySelector("#qtde").addEventListener("change", updatePrice);
+document.querySelector("#js").addEventListener("change", updatePrice);
+document.querySelector("#layout-yes").addEventListener("change", updatePrice);
+document.querySelector("#layout-no").addEventListener("change", updatePrice);
+document.querySelector("#deadline").addEventListener("change", function () {
+    const deadline = document.querySelector("#deadline").value
+    document.querySelector("label[for=deadline]").innerHTML = `Prazo: ${deadline} semanas`
+    updatePrice()
+})
+
+// BUDGET CALCULATION
+function updatePrice(){
+    const qtde = document.querySelector("#qtde").value
+    const hasJS = document.querySelector("#js").checked
+    const includeLayout = document.querySelector("#layout-yes").checked
+    const deadline = document.querySelector("#deadline").value
+    
+    let prize = qtde * 100;
+    if (hasJS) prize *= 1.1
+    if (includeLayout) prize += 500        
+    let urgencyRate = 1 - deadline * 0.1;
+    prize *= 1 + urgencyRate
+
+    document.querySelector("#prize").innerHTML = `R$ ${prize.toFixed(2)}`
+}
